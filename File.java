e75264712942bea46e991fbbd9b5d576022b493e
zmantech <zmantech13@gmail.com>
2024-02-02 09:21:41 -0500

    updated javadoc

--- File.java
+++ File.java
@@ -1,9 +1,22 @@
 import java.io.*;
 import java.util.*; 
 
+/**
+ * all methods should be static.
+ * should be a reader and writer class.
+ */
 public class File {
     //create methods to call which save edit read weight etc
     //this will be what other methods call
 
+    /**
+     * takes in a person and filename and saves the person to string to that file.
+     * @param person    the person to write to the file uses toString method
+     * @param file      the file name to write to
+     * @return true if the write was successful false otherwise
+     */
+    public static boolean writePerson(Person person, String file) {
+        return true;
+    }
 
 }
