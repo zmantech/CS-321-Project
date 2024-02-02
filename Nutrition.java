ba423002f5e329e8f3dd2b6c0e29a218b1ad5212
zmantech <zmantech13@gmail.com>
2024-02-02 09:17:18 -0500

    updated javadoc

--- Nutrition.java
+++ Nutrition.java
@@ -1,5 +1,5 @@
 /**
- * class will be called with weight etc other data needed and pass in BSA etc 
+ * class will be called with weight etc other data needed and pass in BSA etc all methods should be static.
  */
 public class Nutrition {
     
