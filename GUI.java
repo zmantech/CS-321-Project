fcdc145e984c0d974e1ec41a07299403c971f0ce
zmantech <zmantech13@gmail.com>
2024-02-02 09:18:23 -0500

    updated javadoc

--- GUI.java
+++ GUI.java
@@ -1,6 +1,13 @@
+/**
+ * the class that starts and is the interface with the user and all other code.
+ * this should be the only thing with main.
+ */
 public class GUI {
 
-
+    /**
+     * 
+     * @param args command line arguments
+     */
     public static void main(String[] args) {
         //create gui etc here
 
