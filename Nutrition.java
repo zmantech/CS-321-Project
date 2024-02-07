65a425b23382e2debdfca614610dbf0287227f7b
David-EightOneFiveNine <DCoynePrime@gmail.com>
2024-02-07 11:40:46 -0500

    Update Nutrition.java

--- Nutrition.java
+++ Nutrition.java
@@ -25,5 +25,57 @@ public class Nutrition {
         return (float)(person.getWeight() * 0.36);
     }
 
+    /**
+     * this class will get the amount of carbohydrates a person should eat per day.
+     * @param person    the person to check their carbohydrates.
+     * @return          the amount of carbohydrates in grams they should eat per day. 
+     */
+    public static float getCarbohydrates(Person person) {
+        return 0; //placeholder
+    }
 
+    /**
+     * this class will get the amount of Fat a person should eat per day.
+     * @param person    the person to check their Fat.
+     * @return          the amount of Fat in grams they should eat per day.
+     */
+    public static float getFat(Person person) {
+        return 0; //placeholder
+    }
+
+    /**
+     * this class will get the amount of Water a person should drink per day.
+     * @param person    the person to check their Hydration.
+     * @return          the amount of Water in grams they should drink per day.
+     */
+    public static float getHydration(Person person) {
+        return 0; //placeholder
+    }
+
+    /**
+     * this class will get the amount of Fiber a person should eat per day.
+     * @param person    the person to check their Fiber.
+     * @return          the amount of Fiber in grams they should eat per day.
+     */
+    public static float getFiber(Person person) {
+        return 0; //placeholder
+    }
+
+    /**
+     * this class will get the list of Vitamins a person should eat in a day.
+     * @param person    the person to check their Vitamins. *Placeholder to later replace with a structure.
+     * @return          the list of Vitamins in grams-micrograms they should eat per day.
+     */
+    public static float getVitamins(Person person) {  //A double datatype structure(class or list) is needed for all 13+ essential vitamins
+        return 0; //placeholder
+    } //Essential Vitamins per the NCCIH standards: Vitamins A, B(Thiamin, Riboflavin, Niacin, Pantothenic acid, Pyridoxine, Cobalamin, Biotin, Folate Acid), C, D, E, K 
+
+    /**
+     * this class will get the list of Minerals a person should eat in a day.
+     * @param person    the person to check their Minerals. *Placeholder to later replace with a structure.
+     * @return          the amount of Minerals in grams-micrograms they should eat per day.
+     */
+    public static float getMinerals(Person person) { //A double datatype structure(class or list) is needed for all 15+ essential minerals
+        return 0; //placeholder
+    } //Essential Minerals per the NCCIH standards: Calcium, Phosphorus, Potassium, Sodium, Chloride, Magnesium, Iron, Zinc, Iodine, Sulfur, Cobalt, Copper, Fluoride, Magnessium, Selenium
 }
