849cc4416ac860a551a3a37acdd6b1e7770ac303
m-courch <mcourch@gmu.edu>
2024-03-20 13:43:50 -0400

    Create NutritionPanel.java
    This is where the Nutrition Panel will go.

---
+++ NutritionPanel.java
@@ -0,0 +1,14 @@
+public class NutritionPanel {
+    
+    /**
+     * Constructor for the nutrition panel. This will essentially call all methods and create the panel to be put into the nutrition side.
+     * There will be a larger outside panel with the Border Layout.
+     * Each section of the Border layout will have a panel based on what information should go there. Methods will be called by name. e.g(CenterPanel())
+     * Finally, there will be added capability to swap between this panel and the intro panel.
+     * All sections can be added and changed as needed.
+     */
+    public NutritionPanel() {
+
+    }
+
+}
