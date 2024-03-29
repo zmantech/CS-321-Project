d55e0cc5325a7216c0cf00e962602e93a9c815fb
zmantech <zmantech13@gmail.com>
2024-03-29 14:29:54 -0400

    Create Exercise.java

---
+++ Exercise.java
@@ -0,0 +1,18 @@
+/**
+ * the exercise class to find out what exercises people should do to develop a certain muscle.
+ */
+public class Exercise {
+
+    public static void getBicepes() {
+
+    }
+
+    public static void getSideAbs() {
+
+    }
+
+    
+
+
+
+}
