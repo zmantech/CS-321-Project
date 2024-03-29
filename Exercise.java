1d7f61bb8140470961ec90c67b8dc88a15ca8534
zmantech <zmantech13@gmail.com>
2024-03-29 14:33:55 -0400

    Update Exercise.java

--- Exercise.java
+++ Exercise.java
@@ -2,17 +2,36 @@
  * the exercise class to find out what exercises people should do to develop a certain muscle.
  */
 public class Exercise {
+    /**
+     * the exercises the person is gonna do.
+     */
+    private String exercise;
 
-    public static void getBicepes() {
-
+    /**
+     * the amount of the exercise the person should do.
+     */
+    private String amount;
+    /**
+     * creates an exercises object which can hold a string representing the exercise itself and the amout being how much to do.
+     */
+    public Exercise(String exercise, String amount) {
+            this.exercise = exercise;
+            this.amount = amount;
     }
 
-    public static void getSideAbs() {
-
+    /**
+     * the exercise the person should do.
+     * @return the exercise the person should do for that specific muscle.
+     */
+    public String getExercise() {
+        return this.exercise;
     }
 
-    
-
-
-
+    /**
+     * the amount of an exercise a person should do.
+     * @return  a string to print to show how much exercise the person should do.
+     */
+    public String getAmount() {
+        return this.amount;
+    }
 }
