d0b942d6248e51807a3c4cbf587da394b60ee724
zmantech <zmantech13@gmail.com>
2024-03-29 15:01:39 -0400

    Create MuscleExercise.java

---
+++ MuscleExercise.java
@@ -0,0 +1,22 @@
+/**
+ * the class which finds out how much of and what exercise a person should do for a specific muscle.
+ */
+public class MuscleExercise {
+
+    /**
+     * Gets the exercise and the amount for bicepes.
+     * @return an array which holds an object called exercise which has 2 strings the exercise and the amount of the exercise the person should do.
+     */
+    public static Exercise[] getBicepes() {
+
+    }
+
+    /**
+     * Gets the exercise and the amount for forearms.
+     * @return an array which holds an object called exercise which has 2 strings the exercise and the amount of the exercise the person should do.
+     */
+    public static Exercise[] getForearms() {
+
+    }
+    
+}
