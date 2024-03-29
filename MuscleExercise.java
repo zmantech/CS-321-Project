9f94cc5389a96775aa59033426eab2acbb635dc5
zmantech <zmantech13@gmail.com>
2024-03-29 15:22:08 -0400

    Update MuscleExercise.java

--- MuscleExercise.java
+++ MuscleExercise.java
@@ -5,25 +5,34 @@ public class MuscleExercise {
 
     /**
      * Gets the exercise and the amount for cardio.
+     * @oaram type what level the person is in exercising
      * @return an array which holds an object called exercise which has 2 strings the exercise and the amount of the exercise the person should do.
      */
-    public static Exercise[] getCardio() {
+    public static Exercise[] getCardio(Type type) {
+        if(type == Type.INTERMEDIATE) { 
 
+        } else if(type == Type.MODERATE) {
+
+        } else {//advanced
+
+        }
     }
 
     /**
      * Gets the exercise and the amount for weightlifting.
+     * @oaram type what level the person is in exercising
      * @return an array which holds an object called exercise which has 2 strings the exercise and the amount of the exercise the person should do.
      */
-    public static Exercise[] getWeightLifting() {
+    public static Exercise[] getWeightLifting(Type type) {
 
     }
     
     /**
      * Gets the exercise and the amount for sports.
+     * @oaram type what level the person is in exercising
      * @return an array which holds an object called exercise which has 2 strings the exercise and the amount of the exercise the person should do.
      */
-    public static Exercise[] getSports() {
+    public static Exercise[] getSports(Type type) {
 
     }
 }
